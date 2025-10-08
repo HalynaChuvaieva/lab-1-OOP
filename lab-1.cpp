0dcb4107240ad5b6b89dfba58164d91a29499ce5
Halyna <galacuvaeva4@gmail.com>
2025-10-09 00:56:30 +0300

    create vehicle class, make template menu, create file with unit tests

--- lab-1.cpp
+++ lab-1.cpp
@@ -1,11 +1,15 @@
 #include <iostream>
 #include "Graph.h"
 #include "BinarySearchTree.h"
+#include "Vehicle.h"
 using namespace std;
 
-void menu() {
+template <typename T>
+void menu()
+{
     int mainChoice;
-    do {
+    do
+    {
         cout << "\nMAIN MENU\n";
         cout << "1. Binary Search Tree\n";
         cout << "2. Balanced BST\n";
@@ -15,11 +19,14 @@ void menu() {
         cout << "Your choice: ";
         cin >> mainChoice;
 
-        switch (mainChoice) {
-        case 1: {
-            BinarySearchTree<int> bst;
+        switch (mainChoice)
+        {
+        case 1:
+        {
+            BinarySearchTree<T> bst;
             int choice;
-            do {
+            do
+            {
                 cout << "\nBINARY SEARCH TREE MENU\n";
                 cout << "1. Insert\n";
                 cout << "2. Remove\n";
@@ -27,16 +34,31 @@ void menu() {
                 cout << "0. Back\n";
                 cout << "Choice: ";
                 cin >> choice;
-                if (choice == 1) { int val; cout << "Value: "; cin >> val; bst.insert(val); }
-                else if (choice == 2) { int val; cout << "Value: "; cin >> val; bst.remove(val); }
-                else if (choice == 3) bst.display();
+                if (choice == 1)
+                {
+                    T val;
+                    cout << "Value: ";
+                    cin >> val;
+                    bst.insert(val);
+                }
+                else if (choice == 2)
+                {
+                    T val;
+                    cout << "Value: ";
+                    cin >> val;
+                    bst.remove(val);
+                }
+                else if (choice == 3)
+                    bst.display();
             } while (choice != 0);
             break;
         }
-        case 2: {
-            BalancedBST<int> bst;
+        case 2:
+        {
+            BalancedBST<T> bst;
             int choice;
-            do {
+            do
+            {
                 cout << "\nBALANCED BST MENU\n";
                 cout << "1. Insert\n";
                 cout << "2. Remove\n";
@@ -45,29 +67,53 @@ void menu() {
                 cout << "0. Back\n";
                 cout << "Choice: ";
                 cin >> choice;
-                if (choice == 1) { int val; cout << "Value: "; cin >> val; bst.insert(val); }
-                else if (choice == 2) { int val; cout << "Value: "; cin >> val; bst.remove(val); }
-                else if (choice == 3) bst.display();
-                else if (choice == 4) cout << "Height: " << bst.getHeight() << endl;
+                if (choice == 1)
+                {
+                    T val;
+                    cout << "Value: ";
+                    cin >> val;
+                    bst.insert(val);
+                }
+                else if (choice == 2)
+                {
+                    T val;
+                    cout << "Value: ";
+                    cin >> val;
+                    bst.remove(val);
+                }
+                else if (choice == 3)
+                    bst.display();
+                else if (choice == 4)
+                    cout << "Height: " << bst.getHeight() << endl;
             } while (choice != 0);
             break;
         }
-        case 3: {
-            AVLTree<int> avl;
+        case 3:
+        {
+            AVLTree<T> avl;
             int choice;
-            do {
+            do
+            {
                 cout << "\nAVL TREE MENU\n";
                 cout << "1. Insert\n";
                 cout << "2. Display\n";
                 cout << "0. Back\n";
                 cout << "Choice: ";
                 cin >> choice;
-                if (choice == 1) { int val; cout << "Value: "; cin >> val; avl.insert(val); }
-                else if (choice == 2) avl.display();
+                if (choice == 1)
+                {
+                    T val;
+                    cout << "Value: ";
+                    cin >> val;
+                    avl.insert(val);
+                }
+                else if (choice == 2)
+                    avl.display();
             } while (choice != 0);
             break;
         }
-        case 4: {
+        case 4:
+        {
             int graphType;
             cout << "\nGRAPH MENU\n";
             cout << "1. Undirected Graph\n";
@@ -78,33 +124,61 @@ void menu() {
             cin >> graphType;
 
             int n, m;
-            cout << "Enter number of vertices: "; cin >> n;
-            cout << "Enter number of edges: "; cin >> m;
+            cout << "Enter number of vertices: ";
+            cin >> n;
+            cout << "Enter number of edges: ";
+            cin >> m;
 
-            if (graphType == 1) {
-                UndirectedGraph g(n);
+            if (graphType == 1)
+            {
+                UndirectedGraph<T> g(n);
                 cout << "Enter edges (u v):\n";
-                for (int i = 0; i < m; i++) { int u,v; cin >> u >> v; g.addEdge(u,v); }
+                for (int i = 0; i < m; i++)
+                {
+                    int u, v;
+                    cin >> u >> v;
+                    g.addEdge(u, v);
+                }
                 g.buildSpanningTree();
                 cout << "Cyclic? " << (g.isCyclic() ? "Yes\n" : "No\n");
             }
-            else if (graphType == 2) {
-                DirectedGraph g(n);
+            else if (graphType == 2)
+            {
+                DirectedGraph<T> g(n);
                 cout << "Enter edges (u v):\n";
-                for (int i = 0; i < m; i++) { int u,v; cin >> u >> v; g.addEdge(u,v); }
+                for (int i = 0; i < m; i++)
+                {
+                    int u, v;
+                    cin >> u >> v;
+                    g.addEdge(u, v);
+                }
                 g.buildSpanningTree();
                 cout << "Cyclic? " << (g.isCyclic() ? "Yes\n" : "No\n");
             }
-            else if (graphType == 3) {
-                WeightedGraph g(n);
+            else if (graphType == 3)
+            {
+                WeightedGraph<T> g(n);
                 cout << "Enter edges (u v w):\n";
-                for (int i = 0; i < m; i++) { int u,v,w; cin >> u >> v >> w; g.addEdge(u,v,w); }
+                for (int i = 0; i < m; i++)
+                {
+                    int u, v;
+                    T w;
+                    cin >> u >> v >> w;
+                    g.addEdge(u, v, w);
+                }
                 g.dijkstra(0);
             }
-            else if (graphType == 4) {
-                NegativeWeightedGraph g(n);
+            else if (graphType == 4)
+            {
+                NegativeWeightedGraph<T> g(n);
                 cout << "Enter edges (u v w):\n";
-                for (int i = 0; i < m; i++) { int u,v,w; cin >> u >> v >> w; g.addEdge(u,v,w); }
+                for (int i = 0; i < m; i++)
+                {
+                    int u, v;
+                    T w;
+                    cin >> u >> v >> w;
+                    g.addEdge(u, v, w);
+                }
                 g.bellmanFord(0);
             }
 
@@ -114,8 +188,9 @@ void menu() {
     } while (mainChoice != 0);
 }
 
-int main() {
-    menu();
+int main()
+{
+    menu<int>();
+    menu<Vehicle>();
     return 0;
 }
-
